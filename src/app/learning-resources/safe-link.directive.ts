842432867a646e500cb53c9597757e9b2358ea88
Lakshay Jain <Lakshay.Jain@watchguard.com>
2025-01-10 12:10:20 +0530

    more better way to access action through dependency injection

--- src/app/learning-resources/safe-link.directive.ts
+++ src/app/learning-resources/safe-link.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, input } from "@angular/core"
+import { Directive, ElementRef, inject, input } from "@angular/core"
 
 @Directive({
     selector: 'a[appSafeLink]',
     standalone: true,
     host: {
-        '(click)': 'confirmIfUserWantToLeavePage($event)'
+        // '(click)': 'confirmIfUserWantToLeavePage_1($event)'
+        '(click)': 'confirmIfUserWantToLeavePage_2($event)'
     }
 })
 
@@ -14,13 +15,28 @@ export class SafeLinkDirective {
     constructor() {
         console.log('SafeLinkDirective is active');
     }
-    confirmIfUserWantToLeavePage($event: MouseEvent) {
+
+    private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
+    confirmIfUserWantToLeavePage_1($event: MouseEvent) {
         const doesUserWantToLeavePage = window.confirm("Do you really want to leave page ?");
         if (doesUserWantToLeavePage) {
-            const externalLink = (event!.target as HTMLAnchorElement).href;
-            (event!.target as HTMLAnchorElement).href = externalLink + "?from=myapp&" + this.queryParam();
+            const externalLink = (event?.target as HTMLAnchorElement).href;
+            (event?.target as HTMLAnchorElement).href = externalLink + "?from=myapp&" + this.queryParam();
             return;
         }
         event?.preventDefault();
     }
+
+    // this is added after creating hostElementRef varianle and using dependency injection
+    confirmIfUserWantToLeavePage_2($event: MouseEvent) {
+        const doesUserWantToLeavePage = window.confirm("Do you really want to leave page ?");
+        if (doesUserWantToLeavePage) {
+            const externalLink = this.hostElementRef.nativeElement.href;
+            this.hostElementRef.nativeElement.href = externalLink + "?from=myapp&" + this.queryParam();
+            return;
+        }
+        event?.preventDefault();
+    }
+
 }
